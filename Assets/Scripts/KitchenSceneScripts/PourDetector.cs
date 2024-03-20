0a1b205835dabe8aa0a9c66188592693fe771b84
fansxx <fanshixi123@gmail.com>
2024-03-20 23:29:42 +0800

    oil pour wip 1

---
+++ Assets/Scripts/KitchenSceneScripts/PourDetector.cs
@@ -0,0 +1,64 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour
+{
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject streamPrefab = null;
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool pourCheck = CalcaulatePourAngle() < pourThreshold;
+
+        if (isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+
+            if (isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+    }
+
+    private void StartPour()
+    {
+        Debug.Log("start");
+        currentStream = CreateStream();
+        currentStream.Begin();
+    }
+
+    private void EndPour()
+    {
+        Debug.Log("end");
+    }
+
+    private float CalcaulatePourAngle()
+    {
+        return transform.up.y * Mathf.Rad2Deg;
+    }
+
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>();
+    }
+}
+#endif
